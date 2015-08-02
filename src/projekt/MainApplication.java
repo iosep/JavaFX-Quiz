fdb5a4bed18f7987e3ceb9c7ab4736aaf2397502
haerick <hendrikbursian@gmail.com>
2015-08-02 23:10:39 +0200

    Bugfix Spelling

--- src/projekt/MainApplication.java
+++ src/projekt/MainApplication.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 import projekt.controller.ScreenController;
 
 public class MainApplication extends Application {
-    public static final String TITLE = "Scratchers";
+    public static final String TITLE = "Scratcherz";
     public static final String PATH_PLAYER_IMAGES = "projekt/data/img/player/";
 
     @Override
